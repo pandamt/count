f43b563be3d3828e58ada2436c9547cdfbe4f286
Mengting Fang <mtfang0707@gmail.com>
2021-01-08 16:31:33 -0500

    Create model_settings.py

---
+++ model_settings.py
@@ -0,0 +1,13 @@
+img_height = 50
+img_width = 150
+min_edge = 4
+max_edge = 6
+min_blobs_train = 1
+max_blobs_train = 15
+min_blobs_test = 1
+max_blobs_test = 9
+
+
+glimpses = max_blobs_train - min_blobs_train + 2
+learning_rate = 1e-2
+batch_size = 1
