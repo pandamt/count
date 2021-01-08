347320ce7e91d2536901941044993fe2ccb5106e
Mengting Fang <mtfang0707@gmail.com>
2021-01-08 16:56:18 -0500

    Update create_data_count.py

--- create_data_count.py
+++ create_data_count.py
@@ -2,16 +2,16 @@
 import sys
 import numpy as np
 import random
-from model_settings import img_height, img_width, p_size, glimpses, min_edge, max_edge, min_blobs_train, max_blobs_train, min_blobs_test, max_blobs_test # MT 
+from model_settings import img_height, img_width, glimpses, min_edge, max_edge, min_blobs_train, max_blobs_train, min_blobs_test, max_blobs_test # MT 
 
-def set_size(num_blobs, even=None):
+def set_size(num_blobs, testing=False):
     """Get amount of images for each number"""
-    if even is None:
-        return int(10000/(num_blobs**2)) # uneven: number distribution 1/(n^2)
+    if testing is False:
+        return int(10000/(num_blobs**2)) # training: number distribution 1/(n^2)
     else:
-        return 1000 # even: make 1000 images for each number
+        return 1000 # testing: make 1000 images for each number
 
-def get_total(even, min_blobs, max_blobs):
+def get_total(testing, min_blobs, max_blobs):
     """Get total amount of images"""
     c = min_blobs
     total = 0
@@ -20,15 +20,39 @@ def get_total(even, min_blobs, max_blobs):
        c = c + 1
     return total
 
+def get_dims(testing, min_edge, max_edge):
+     """Get the dimensions of each blob"""
+ 
+     if testing:
+         width = 5
+     else:
+        width = random.randint(min_edge, max_edge)
+ 
+     height = width # square blobs
+ 
+     return width, height
+
+def get_coordinate(testing):
+     """Get the coordinates of the top left corner for each blob"""
+ 
+     if testing:
+         cX_ = random.randint(8, 9)
+         cY_ = 3
+     else:
+         cX_ = random.randint(7, 10)
+         cY_ = 10
+ 
+     return cX_, cY_
+
 def get_mask(num_blobs):
     mask = np.zeros(glimpses)
     for i in range (num_blobs+1):
         mask[i] = 1
     return mask
 
-def generate_data(even, min_blobs, max_blobs): # MT     
+def generate_data(testing, min_blobs, max_blobs): # MT     
     n_labels = max_blobs_train - min_blobs_train + 1 # MT
-    total = get_total(even, min_blobs, max_blobs)
+    total = get_total(testing, min_blobs, max_blobs)
     imgs = np.zeros([total, img_height*img_width]) # input img size
     labels = np.zeros([total, n_labels])
     blob_list = np.zeros([total, glimpses, 2])
@@ -41,41 +65,40 @@ def generate_data(even, min_blobs, max_blobs): # MT
 
     while (num_blobs < max_blobs + 1):
 
-        nOfItem = set_size(num_blobs, even) # even, 1000; uneven, 10000/(num_blobs**2)
+        nOfItem = set_size(num_blobs, testing) # testing, 1000; training, 10000/(num_blobs**2)
 
         for i in range(nOfItem):
             img = np.ones(img_height*img_width) # input img size # MT: change zeros to ones
             num_count = 0
             glimpse_count = 0
             cX_prev = 0
+            cY_prev = 0
 
             while num_count < num_blobs:
-                height = random.randint(min_edge, max_edge)
-                #width = random.randint(min_edge, max_edge)
-                width = height # for square blobs
+                width, height = get_dims(testing, min_edge, max_edge)
+                cX_, cY_ = get_coordinate(testing)
                 if num_count == 0:
-                    cX = random.randint(1, 10) # 9 blobs
-                    # cX = random.randint(15, 25) # 5 blobs
+                    cX = cX_
                 else:
-                    cX = cX_prev + random.randint(5, 10) # 9 blobs
-                    # cX = cX_prev + random.randint(15, 20) # 9 blobs
-                cY = random.randint(img_height/2 - 15, img_height/2 + 15 - height) # -15 ~ 15
-                blob_list[img_count, glimpse_count, 0] = cX + int(width/2) # top left corner
-                blob_list[img_count, glimpse_count, 1] = cY + int(height/2)
+                    cX = cX_prev + cX_
+                cY = random.randint(img_height/2 - cY_, img_height/2 + cY_ - height) # -10~10 / -3~3
+                blob_list[img_count, glimpse_count, 0] = cX + width/2 # lower left corner
+                blob_list[img_count, glimpse_count, 1] = cY + height/2
                 size_list[img_count, glimpse_count] = height
                 count_word[img_count, glimpse_count, num_count + 1] = 1 # "I'm done!":[1,0,0,0,0], "one":[0,1,0,0,0], "two":[0,0,1,0,0] 
                 cX_prev = cX
+                cY_prev = cY
 
                 for p in range(cY, cY+height):
                     for q in range(cX, cX+width):
-                        img[p*img_width+q] = 155
+                        img[p*img_width+q] = 255
 
                 num_count += 1
                 glimpse_count += 1
 
             # I'm done!
-            blob_list[img_count, glimpse_count:glimpses, 0] = img_width - p_size/2
-            blob_list[img_count, glimpse_count:glimpses, 1] = random.randint(img_height/2 - 15, img_height/2 + 15 - height) + int(height/2)
+            blob_list[img_count, glimpse_count:glimpses, 0] = cX_prev+width/2
+            blob_list[img_count, glimpse_count:glimpses, 1] = cY_prev+height/2
             count_word[img_count, glimpse_count:glimpses, 0] = 1
 
             imgs[img_count] = img
@@ -95,3 +118,40 @@ def generate_data(even, min_blobs, max_blobs): # MT
 #            label = np.vstack((label, empty_label))
 
     return imgs, labels, blob_list, size_list, mask_list, num_list, count_word
+
+def generate_blank_img(testing, min_blobs, max_blobs): # MT     
+    n_labels = max_blobs_train - min_blobs_train + 1 # MT
+    total = get_total(testing, min_blobs, max_blobs)
+    imgs = np.zeros([total, img_height*img_width]) # input img size
+    labels = np.zeros([total, n_labels])
+    blob_list = np.zeros([total, glimpses, 2])
+    size_list = np.zeros([total, glimpses])
+    mask_list = np.zeros([total, glimpses])
+    num_list = np.zeros(total)
+    count_word = np.zeros([total, glimpses, n_labels+1])
+    num_blobs = min_blobs
+    img_count = 0
+
+    for i in range(total):
+        img = np.ones(img_height*img_width) # input img size # MT: change zeros to ones
+        glimpse_count = 0
+        num_count = 0  
+        while (glimpse_count < glimpses):
+            if num_count < n_labels:
+                count_word[img_count, glimpse_count, num_count + 1] = 1 # "one":[0,1,0,0,0], "two":[0,0,1,0,0] 
+            else:
+                count_word[img_count, glimpse_count, 0] = 1 # "I'm done!":[1,0,0,0,0]
+                
+            glimpse_count += 1
+            num_count += 1
+            
+        imgs[img_count] = img
+        labels[img_count, n_labels-1] = 1
+        mask_list[img_count] = get_mask(n_labels)
+        num_list[img_count] = n_labels
+        img_count = img_count + 1
+
+    np.set_printoptions(threshold=np.nan)
+
+    return imgs, labels, blob_list, size_list, mask_list, num_list, count_word
+
